6dd7420bf991207e09bc5b35c255c5e2faf27a23
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-11-03 15:54:57 +0700

    GT

---
+++ PT16304_UD_COM108_NhapMonLapTrinh/Bai_1.0_GioiThieuLapTrinhC/Main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+/// <summary>
+/// Ctrl + Space dùng để gợi ý code khi lập trình
+/// F5 Dùng để chạy chương trình
+/// 1 Project luôn chỉ có 1 hàm main ở bên trogn
+/// Printf("Điền dữ liệu muốn in ra màn hình");
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	printf("Chao Cac Ban Den Voi Nhap Mon LT C CAN BAN");
+	return 0;
+}
